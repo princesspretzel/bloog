1f3452971cf53e891ea951c42af2495f6661afa4
princesspretzel <brittanga@gmail.com>
2013-12-20 16:53:01 -0500

    bloog set-up

--- config.rb
+++ config.rb
@@ -1,8 +1,25 @@
-# establishes our connection to the DB and other settings
-ActiveRecord::Base.establish_connection(
-    :adapter => "postgresql",
-    :host => "localhost",
-    :username => "omardelarosa",
-    :database => "simple_blog",
-    :encoding => "utf8"
+configure :development do 
+
+  ActiveRecord::Base.establish_connection(
+    adapter: "postgresql",
+    host: "localhost",
+    username: "Banana",
+    database: "bloog",
+    encoding: "utf8"
   )
+
+end
+
+configure :production do 
+ DATABASE_URL:  HEROKU_POSTGRESQL_CYAN_URL 
+
+  ActiveRecord::Base.establish_connection(
+    adapter: "postgresql",
+    host: "ec2-54-204-43-139.compute-1.amazonaws.com",
+    username: "jvxcvzjvhmszkc",
+    password: "18YutazGa2nsGc42jMvBDmH0Xc",
+    database: "db7ofuc7kfq0k5",
+    port: "5432"
+  )
+
+end
